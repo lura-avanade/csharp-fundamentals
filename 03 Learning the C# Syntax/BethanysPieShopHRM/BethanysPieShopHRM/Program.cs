6923c14ba622156d160a70a04dc70824b6a50672
Luis Ramirez Redondo <luis.ramirez.redondo@avanade.com>
2024-09-10 14:21:15 +0200

    working with primitive types and constants values

--- 03 Learning the C# Syntax/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
+++ 03 Learning the C# Syntax/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
@@ -4,3 +4,33 @@ Console.WriteLine("Welcome to Bethany's Pie Shop HRM!");
 Console.WriteLine("Please enter your name: ");
 
 string name = Console.ReadLine();
+
+string name_2 = Console.ReadLine();
+
+string Name_2 = Console.ReadLine();
+
+string 2Name = Console.ReadLine();
+
+int monthlyWage = 1234;
+
+int months = 12, bonus = 1000;
+
+bool isActive = true;
+
+double rating = 99.25;
+
+//byte numberOfEmployees = 300;
+
+int hoursWorked;
+
+hoursWorked = 125;
+
+hoursWorked = 148;
+
+//monthlyWage = true;
+
+const double interestRate = 0.07;
+
+//interestRate = 0.08;
+
+
