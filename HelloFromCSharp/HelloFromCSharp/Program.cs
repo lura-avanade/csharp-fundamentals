29095dae32230eb789ce1f96e4e0d4ef0bee084b
Luis Ramirez Redondo <luis.ramirez.redondo@avanade.com>
2024-09-10 11:10:56 +0200

    debugging in visual studio

--- HelloFromCSharp/HelloFromCSharp/Program.cs
+++ HelloFromCSharp/HelloFromCSharp/Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello everybody!");
 
-Console.WriteLine("Please enter your name: ");
+Console.WriteLine("Please enter your first name: ");
+string firstName = Console.ReadLine();
 
-string name = Console.ReadLine();
-Console.WriteLine("Hello, " + name);
+Console.WriteLine("Please enter your last name: ");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("Hello " + firstName + " " + lastName);
