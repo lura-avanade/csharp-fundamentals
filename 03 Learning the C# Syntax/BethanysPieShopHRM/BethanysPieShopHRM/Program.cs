3c98adecd7e77556e3365dc8fa61641cae334607
Luis Ramirez Redondo <luis.ramirez.redondo@avanade.com>
2024-09-10 14:51:19 +0200

    demo: creating strings

--- 03 Learning the C# Syntax/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
+++ 03 Learning the C# Syntax/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
@@ -1,15 +1,15 @@
 ﻿
-Console.WriteLine("Welcome to Bethany's Pie Shop HRM!");
+//Console.WriteLine("Welcome to Bethany's Pie Shop HRM!");
 
-Console.WriteLine("Please enter your name: ");
+//Console.WriteLine("Please enter your name: ");
 
-string name = Console.ReadLine();
+//string name = Console.ReadLine();
 
-string name_2 = Console.ReadLine();
+//string name_2 = Console.ReadLine();
 
-string Name_2 = Console.ReadLine();
+//string Name_2 = Console.ReadLine();
 
-string 2Name = Console.ReadLine();
+//string 2Name = Console.ReadLine();
 
 int monthlyWage = 1234;
 
@@ -33,4 +33,10 @@ const double interestRate = 0.07;
 
 //interestRate = 0.08;
 
+string firstName = "Bethany";
+string lastName = "Smith";
 
+string emptyString = "";
+
+Console.WriteLine("Please enter your name");
+string name = Console.ReadLine();
