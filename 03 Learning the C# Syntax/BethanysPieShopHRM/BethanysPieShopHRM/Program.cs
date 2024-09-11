ee9d1843be720f4474e75cc1588293e27f0b1f47
Luis Ramirez Redondo <luis.ramirez.redondo@avanade.com>
2024-09-11 11:15:28 +0200

    demo: working with relational operators

--- 03 Learning the C# Syntax/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
+++ 03 Learning the C# Syntax/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
@@ -1,103 +1,20 @@
 ﻿
-Console.WriteLine("Welcome to Bethany's Pie Shop HRM!");
+int age = 23;
 
-//Console.WriteLine("Please enter your name: ");
+bool a = age == 23;
+Console.WriteLine("Age is 23: " + a);
 
-//string name = Console.ReadLine();
+bool b = age > 23;
+Console.WriteLine("Age is greater than 23: " + b);
 
-//string name_2 = Console.ReadLine();
+bool c = (age >= 18) && (age <= 65);
+Console.WriteLine("Age is between 18 and 65: " + c);
 
-//string Name_2 = Console.ReadLine();
 
-//string 2Name = Console.ReadLine();
+int age1 = 16;
+int age2 = 64;
 
-var monthlyWage = 1234;
-
-int months = 12, bonus = 1000;
-
-var isActive = true;
-
-var rating = 99.25;
-
-var numberOfEmployees = 300;
-
-//int hoursWorked;
-
-//hoursWorked = 125;
-
-//hoursWorked = 148;
-
-////monthlyWage = true;
-
-//const double interestRate = 0.07;
-
-////interestRate = 0.08;
-
-//string firstName = "Bethany";
-//string lastName = "Smith";
-
-//string emptyString = "";
-
-//Console.WriteLine("Please enter your name");
-//string name = Console.ReadLine();
-
-//double ratePerHour = 12.34;
-//int numberOfHoursWorked = 165;
-
-//double currentMonthWage = ratePerHour * numberOfHoursWorked + bonus;
-//Console.WriteLine(currentMonthWage);
-
-//ratePerHour += 3; //ratePerHour = ratePerHour + 3;
-//Console.WriteLine(ratePerHour);
-
-//if (currentMonthWage > 2000)
-//    Console.WriteLine("Top paid employee!");
-
-//int numberOfEmployees = 15;
-//numberOfEmployees--; //++
-//Console.WriteLine(numberOfEmployees);
-
-//bool a;
-//int b;
-
-//int intMaxValue = int.MaxValue;
-//int intMinValue = int.MinValue;
-
-//char userSelection = 'a';
-//char upperVersion = char.ToUpper(userSelection);
-
-//bool isDigit = char.IsDigit(userSelection);
-
-//bool isLetter = char.IsLetter(userSelection);
-
-var hireDate = new DateTime(2022, 3, 28, 14, 30, 0);
-//Console.WriteLine(hireDate);
-
-//DateTime exitDate = new DateTime(2025,12,11);
-
-////DateTime invalidDate = new DateTime(2025,15,11);
-
-//DateTime startDate = hireDate.AddDays(15);
-//Console.WriteLine(startDate);
-
-//DateTime currentDate= DateTime.Now;
-//bool areWeInDst = currentDate.IsDaylightSavingTime();
-
-//DateTime startHour = DateTime.Now;
-//TimeSpan workTime = new TimeSpan(8, 35, 0);
-//DateTime endHour = startHour.Add(workTime);
-
-//Console.WriteLine(startHour.ToLongDateString());
-//Console.WriteLine(endHour.ToShortTimeString());
-
-int numberOfHoursWorked = 165;
-
-long veryLongMonth = numberOfHoursWorked; //works fine
-
-double d = 123456789.0;
-
-int x = (int)d;
-
-int intVeryLongMonth = (int)veryLongMonth;
-
-Console.ReadLine();
+bool d = (age1 >= 18) && (age2 <= 65);
+Console.WriteLine("Age1 is greater than 18 AND age2 is less than 65: " + d);
+bool e = (age1 >= 18) || (age2 <= 65);
+Console.WriteLine("Age1 is greater than 18 OR age2 is less than 65: " + e);
