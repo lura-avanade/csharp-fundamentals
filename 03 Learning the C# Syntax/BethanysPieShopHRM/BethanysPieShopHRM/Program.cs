333fd7c7eda56df8f8e14165dfa039ac6280a4d4
Luis Ramirez Redondo <luis.ramirez.redondo@avanade.com>
2024-09-11 09:17:30 +0200

    demo: working with datetime

--- 03 Learning the C# Syntax/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
+++ 03 Learning the C# Syntax/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
@@ -13,7 +13,7 @@ Console.WriteLine("Welcome to Bethany's Pie Shop HRM!");
 
 //int monthlyWage = 1234;
 
-int months = 12, bonus = 1000;
+//int months = 12, bonus = 1000;
 
 //bool isActive = true;
 
@@ -60,14 +60,34 @@ int months = 12, bonus = 1000;
 //bool a;
 //int b;
 
-int intMaxValue = int.MaxValue;
-int intMinValue = int.MinValue;
+//int intMaxValue = int.MaxValue;
+//int intMinValue = int.MinValue;
 
-char userSelection = 'a';
-char upperVersion = char.ToUpper(userSelection);
+//char userSelection = 'a';
+//char upperVersion = char.ToUpper(userSelection);
 
-bool isDigit = char.IsDigit(userSelection);
+//bool isDigit = char.IsDigit(userSelection);
 
-bool isLetter = char.IsLetter(userSelection);
+//bool isLetter = char.IsLetter(userSelection);
+
+DateTime hireDate = new DateTime(2022,3,28,14,30,0);
+Console.WriteLine(hireDate);
+
+DateTime exitDate = new DateTime(2025,12,11);
+
+//DateTime invalidDate = new DateTime(2025,15,11);
+
+DateTime startDate = hireDate.AddDays(15);
+Console.WriteLine(startDate);
+
+DateTime currentDate= DateTime.Now;
+bool areWeInDst = currentDate.IsDaylightSavingTime();
+
+DateTime startHour = DateTime.Now;
+TimeSpan workTime = new TimeSpan(8, 35, 0);
+DateTime endHour = startHour.Add(workTime);
+
+Console.WriteLine(startHour.ToLongDateString());
+Console.WriteLine(endHour.ToShortTimeString());
 
 Console.ReadLine();
