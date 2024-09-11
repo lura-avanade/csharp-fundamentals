f45109bb73e02a69ff8af23c52ccfab97c5c2261
Luis Ramirez Redondo <luis.ramirez.redondo@avanade.com>
2024-09-11 08:26:17 +0200

    c# operators

--- 03 Learning the C# Syntax/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
+++ 03 Learning the C# Syntax/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-//Console.WriteLine("Welcome to Bethany's Pie Shop HRM!");
+Console.WriteLine("Welcome to Bethany's Pie Shop HRM!");
 
 //Console.WriteLine("Please enter your name: ");
 
@@ -11,32 +11,53 @@
 
 //string 2Name = Console.ReadLine();
 
-int monthlyWage = 1234;
+//int monthlyWage = 1234;
 
 int months = 12, bonus = 1000;
 
-bool isActive = true;
+//bool isActive = true;
 
-double rating = 99.25;
+//double rating = 99.25;
 
-//byte numberOfEmployees = 300;
+////byte numberOfEmployees = 300;
 
-int hoursWorked;
+//int hoursWorked;
 
-hoursWorked = 125;
+//hoursWorked = 125;
 
-hoursWorked = 148;
+//hoursWorked = 148;
 
-//monthlyWage = true;
+////monthlyWage = true;
 
-const double interestRate = 0.07;
+//const double interestRate = 0.07;
 
-//interestRate = 0.08;
+////interestRate = 0.08;
 
-string firstName = "Bethany";
-string lastName = "Smith";
+//string firstName = "Bethany";
+//string lastName = "Smith";
 
-string emptyString = "";
+//string emptyString = "";
 
-Console.WriteLine("Please enter your name");
-string name = Console.ReadLine();
+//Console.WriteLine("Please enter your name");
+//string name = Console.ReadLine();
+
+double ratePerHour = 12.34;
+int numberOfHoursWorked = 165;
+
+double currentMonthWage = ratePerHour * numberOfHoursWorked + bonus;
+Console.WriteLine(currentMonthWage);
+
+ratePerHour += 3; //ratePerHour = ratePerHour + 3;
+Console.WriteLine(ratePerHour);
+
+if (currentMonthWage > 2000)
+    Console.WriteLine("Top paid employee!");
+
+int numberOfEmployees = 15;
+numberOfEmployees--; //++
+Console.WriteLine(numberOfEmployees);
+
+bool a;
+int b;
+
+Console.ReadLine();
