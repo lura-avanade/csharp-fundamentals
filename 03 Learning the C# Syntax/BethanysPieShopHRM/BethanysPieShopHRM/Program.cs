f5679d81a0aa574d9ce0fe889dd07af431815498
Luis Ramirez Redondo <luis.ramirez.redondo@avanade.com>
2024-09-11 14:16:26 +0200

    demo: more loops. BIS

--- 03 Learning the C# Syntax/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
+++ 03 Learning the C# Syntax/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
@@ -95,35 +95,35 @@
 //    selectedAction = Console.ReadLine();
 //}
 
-string selectedAction = string.Empty;
+//string selectedAction = string.Empty;
 
-do
-{
-    Console.WriteLine("Choose the action you want to do: ");
-    Console.WriteLine("1. Add employee");
-    Console.WriteLine("2. Update employee");
-    Console.WriteLine("3. Delete employee");
-    Console.WriteLine("99. Exit application");
-    selectedAction = Console.ReadLine();
+//do
+//{
+//    Console.WriteLine("Choose the action you want to do: ");
+//    Console.WriteLine("1. Add employee");
+//    Console.WriteLine("2. Update employee");
+//    Console.WriteLine("3. Delete employee");
+//    Console.WriteLine("99. Exit application");
+//    selectedAction = Console.ReadLine();
 
-    switch (selectedAction)
-    {
-        case "1":
-            Console.WriteLine("Adding new employee...");
-            break;
-        case "2":
-            Console.WriteLine("Updating employee...");
-            break;
-        case "3":
-            Console.WriteLine("Deleting employee...");
-            break;
-        default:
-            Console.WriteLine("Invalid input");
-            break;
-    }
-} while (selectedAction != "99");
+//    switch (selectedAction)
+//    {
+//        case "1":
+//            Console.WriteLine("Adding new employee...");
+//            break;
+//        case "2":
+//            Console.WriteLine("Updating employee...");
+//            break;
+//        case "3":
+//            Console.WriteLine("Deleting employee...");
+//            break;
+//        default:
+//            Console.WriteLine("Invalid input");
+//            break;
+//    }
+//} while (selectedAction != "99");
 
-Console.WriteLine("Closing application");
+//Console.WriteLine("Closing application");
 
 //Console.WriteLine("Enter a value: ");
 //int max = int.Parse(Console.ReadLine());
@@ -163,7 +163,19 @@ Console.WriteLine("Closing application");
 //    Console.WriteLine(DateTime.Now);
 //}
 
-for (int i = 0; i < 10; i++) 
+//for (int i = 0; i < 10; i++) 
+//{
+//    Console.WriteLine(i);
+//}
+
+Console.WriteLine("Enter a value: ");
+int max = int.Parse(Console.ReadLine());
+for (int i = 0; i < max; i++)
 {
+    if (i == 5) 
+    {
+        Console.WriteLine("Bingo! " + i + " was found!");
+        continue;
+    }
     Console.WriteLine(i);
 }
