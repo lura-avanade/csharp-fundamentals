e93cc2eb494345ad68c72bc7217d586fbf17fe47
Luis Ramirez Redondo <luis.ramirez.redondo@avanade.com>
2024-09-11 13:24:16 +0200

    demo: using if statements

--- 03 Learning the C# Syntax/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
+++ 03 Learning the C# Syntax/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
@@ -1,20 +1,38 @@
 ﻿
-int age = 23;
+//int age = 23;
 
-bool a = age == 23;
-Console.WriteLine("Age is 23: " + a);
+//bool a = age == 23;
+//Console.WriteLine("Age is 23: " + a);
 
-bool b = age > 23;
-Console.WriteLine("Age is greater than 23: " + b);
+//bool b = age > 23;
+//Console.WriteLine("Age is greater than 23: " + b);
 
-bool c = (age >= 18) && (age <= 65);
-Console.WriteLine("Age is between 18 and 65: " + c);
+//bool c = (age >= 18) && (age <= 65);
+//Console.WriteLine("Age is between 18 and 65: " + c);
 
 
-int age1 = 16;
-int age2 = 64;
+//int age1 = 16;
+//int age2 = 64;
 
-bool d = (age1 >= 18) && (age2 <= 65);
-Console.WriteLine("Age1 is greater than 18 AND age2 is less than 65: " + d);
-bool e = (age1 >= 18) || (age2 <= 65);
-Console.WriteLine("Age1 is greater than 18 OR age2 is less than 65: " + e);
+//bool d = (age1 >= 18) && (age2 <= 65);
+//Console.WriteLine("Age1 is greater than 18 AND age2 is less than 65: " + d);
+//bool e = (age1 >= 18) || (age2 <= 65);
+//Console.WriteLine("Age1 is greater than 18 OR age2 is less than 65: " + e);
+
+Console.WriteLine("Enter the age of the new candidate: ");
+int age = int.Parse(Console.ReadLine());
+
+if (age < 18)
+{
+    Console.WriteLine("Too young to apply");
+    Console.WriteLine("Send email to candidate.");
+}
+else if (age > 65) 
+{
+    Console.WriteLine("Sorry, the selected age is too old");
+    Console.WriteLine("Send email to candidate.");
+}
+else
+{
+    Console.WriteLine("Great, you can now start with the application!");
+}
