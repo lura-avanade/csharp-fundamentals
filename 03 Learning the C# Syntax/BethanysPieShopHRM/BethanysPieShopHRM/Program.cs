5e84a5e8253abf2a2bf4203c9c156efa981a0548
Luis Ramirez Redondo <luis.ramirez.redondo@avanade.com>
2024-09-11 14:47:02 +0200

    demo: adding a helper file

--- 03 Learning the C# Syntax/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
+++ 03 Learning the C# Syntax/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
@@ -1,21 +1,11 @@
 ﻿
+using BethanysPieShopHRM;
+
 int amount = 1234;
 int months = 12;
 
-int yearlyWage = CalculateYearlyWage(amount, months);
+int yearlyWage = Utilities.CalculateYearlyWage(amount, months);
 
 Console.WriteLine($"Yearly wage: {yearlyWage}");
 
 Console.ReadLine();
-
-
-static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
-{
-    //Console.WriteLine($"Yearly wage: {monthlyWage * numberOfMonthsWorked}");
-    //return monthlyWage * numberOfMonthsWorked;
-
-    if (numberOfMonthsWorked == 12)//let's add bonus month
-        return monthlyWage * (numberOfMonthsWorked + 1);
-
-    return monthlyWage * numberOfMonthsWorked;
-}
---
+++ 03 Learning the C# Syntax/BethanysPieShopHRM/BethanysPieShopHRM/Utilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM
+{
+    internal class Utilities
+    {
+        public static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
+        {
+            //Console.WriteLine($"Yearly wage: {monthlyWage * numberOfMonthsWorked}");
+            //return monthlyWage * numberOfMonthsWorked;
+
+            if (numberOfMonthsWorked == 12)//let's add bonus month
+                return monthlyWage * (numberOfMonthsWorked + 1);
+
+            return monthlyWage * numberOfMonthsWorked;
+        }
+    }
+}
